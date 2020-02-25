4805a389b3d8e67c9c3bcb3d7cebedf07dce6012
Teresa Liu <kuas033@gmail.com>
2020-02-24 16:00:50 -0800

    connected to local host

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-const express = require ('express');
+'use strict';
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const expressHandlebars = require('express-handlebars');
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgers_controller');
+
+app.use(routes);
+
+app.listen(PORT, () =>
+    console.log(`Server listening on: http://localhost:${PORT}`)
+);
